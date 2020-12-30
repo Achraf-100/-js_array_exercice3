a3f138f0eb10fadd21219f4b9bcb1ac058764235
Achraf <hardy1348@live.fr>
2020-12-30 15:10:39 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+let fruits = ["pomme", "cerise", "banane"];
+fruits.forEach(element => console.log(element));
+
+let nom = fruits.push("Achraf")
+console.log(fruits);
+
+let nouveau = 0
+fruits[nouveau] = 'fraise'
+console.log(fruits);
+
